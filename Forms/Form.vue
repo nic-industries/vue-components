cd0b44e81d06b68d444035300644179507758855
Sebastian Inman <sebastian@nicindustries.com>
2020-06-19 09:20:35 -0700

    Added Form component

---
+++ Forms/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form class="flex-column" @submit="submit">
+    <slot></slot>
+  </form>
+</template>
+
+<script lang="ts">
+  import {Component, Emit, Prop, Vue} from "vue-property-decorator";
+  @Component export default class Form extends Vue {
+    @Emit() submit($event: any) {
+      return $event;
+    }
+  }
+</script>
