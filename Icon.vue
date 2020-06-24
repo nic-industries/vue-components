d63331ca7e28dc2bf667a7da42f51fa67b1010df
Sebastian Inman <sebastian@nicindustries.com>
2020-06-24 09:28:42 -0700

    Added Icon component

---
+++ Icon.vue
@@ -0,0 +1,37 @@
+<template>
+  <i :class="classes"></i>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator";
+  @Component export default class Icon extends Vue {
+
+    @Prop({default: 'fa'}) family!: string;
+    @Prop({default: 'currentColor'}) color!: string;
+    @Prop({default: 4}) scale!: number;
+
+    get classes() {
+
+      let classes = [this.family, `fa-${this.icon}`];
+
+      if(this.scale) classes.push(`font-size-${this.scale}`);
+      if(this.color !== 'currentColor') classes.push(`color-${this.color}`);
+
+      return classes.join(' ');
+    }
+
+    get icon() {
+
+      let slots: any = this.$slots;
+
+      return slots.default[0].text;
+    }
+
+  }
+</script>
+
+<style lang="scss">
+  i {
+    color: currentColor;
+  }
+</style>
