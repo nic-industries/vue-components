a1652b5568464ff57025e10898b57efea158479a
Sebastian Inman <sebastian@nicindustries.com>
2020-06-29 15:08:28 -0700

    Added VerticalSpacer component

---
+++ VerticalSpacer.vue
@@ -0,0 +1,30 @@
+<template>
+  <hr :class="classes" />
+</template>
+
+<script lang="ts">
+
+  import {Component, Emit, Prop, Vue} from "vue-property-decorator";
+
+
+  /**
+   * @class Spacer
+   * @description Vue component for displaying a vertical spacer element.
+   * @property height The size multiplier that determines the height of the spacer.
+   * @example <VerticalSpacer height="4" />
+   */
+
+  @Component export default class VerticalSpacer extends Vue {
+
+    @Prop({ default: 4 }) height!: number;
+
+    get classes() {
+
+      let classes = ['display-flex', 'mt-0', `mb-${this.height}`];
+      return classes.join(" ");
+
+    }
+
+  }
+
+</script>
