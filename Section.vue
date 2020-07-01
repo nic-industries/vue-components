9742461a367cd591004ddd0aa59214f040b86ad8
Sebastian Inman <sebastian@nicindustries.com>
2020-07-01 13:40:58 -0700

    Added section title option

--- Section.vue
+++ Section.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="section">
     <div class="section-content">
+      <div v-if="title" class="section-title flex flex-row items-flex-end mb-4">
+        <h2 class="mb-0">{{title}}</h2>
+        <a v-if="link" :href="link">{{linkText}}</a>
+      </div>
       <slot></slot>
     </div>
   </section>
@@ -9,7 +13,11 @@
 
 <script lang="ts">
   import {Component, Prop, Vue} from "vue-property-decorator";
-  @Component export default class Section extends Vue {}
+  @Component export default class Section extends Vue {
+    @Prop() title!: string;
+    @Prop() link!: string;
+    @Prop() linkText!: string;
+  }
 </script>
 
 
@@ -21,6 +29,16 @@
     width: 100%;
     &:not(:last-child) {
       @include Convert\Pixel-Rem(margin-bottom, -16px);
+    }
+    &-title {
+      > h2, > .h2 {
+        line-height: 1;
+      }
+      > a {
+        @include Convert\Pixel-Rem(margin-left, 25px);
+        @include Convert\Pixel-Rem(font-size, 14px);
+        font-weight: 600;
+      }
     }
     &-content {
       flex-direction: column;
